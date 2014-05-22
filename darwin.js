22f612f79ce94942dac5e14680a5c50734d64826
David Robles <drobles@gmail.com>
2014-05-22 08:50:47 -0700

    Basic solver for Hello World problem

--- darwin.js
+++ darwin.js
@@ -16,6 +16,15 @@ function randomTopPercent(evaluatedPopulation) {
     return evaluatedPopulation[intValue];
 }
 
+function reproduce(x, y) {
+    // verify certain initial condition
+    var c = randomIntFromInterval(0, x.length);
+    var first = x.substr(0, c) + y.substr(c, x.length);
+    // var second = x.substr(0, c) + y.substr(c, x.length);
+    // return [first, second];
+    return first;
+}
+
 /**
  * Generates a population of popSize individuals based
  * in the genIndfunc function.
@@ -53,19 +62,30 @@ function evaluatePopulation(population, fitnessFunction) {
     return evaluatedPopulation;
 }
 
+// add mutation
+
 function run_ga(numGens, genIndFunc, popSize, fitnessFunction) {
     if (numGens < 1) {
         throw Error("Number of generations must be at least 1.");
     }
     population = generatePopulation(genIndFunc, popSize);
     for (var i = 0; i < numGens; i++) {
+        console.log('-------------------');
         console.log("Generation " + (i + 1));
+        newPopulation = [];
         var evaluatedPopulation = evaluatePopulation(population, fitnessFunction);
         evaluatedPopulation.sort(function(a, b) {
-            return a.fitness - b.fitness;
+            return b.fitness - a.fitness;
         });
-        x = randomTopPercent(evaluatedPopulation);
-        y = randomTopPercent(evaluatedPopulation);
+        for (var j = 0; j < population.length; j++) {
+            var x = randomTopPercent(evaluatedPopulation);
+            var y = randomTopPercent(evaluatedPopulation);
+            var child = reproduce(x.candidate, y.candidate); // remove .candidate
+            newPopulation.push(child);
+        }
+        population = newPopulation;
+        console.log('Best: ' + population[0]);
+        console.log('Fitness: ' + fitnessFunction(population[0]));
     }
 }
 
@@ -81,10 +101,10 @@ function createRandomWordGenerator(wordLength) {
     }
 }
 
-var numGens = 10,
-    popSize = 10,
+var numGens = 50,
+    popSize = 100,
     genIndFunc = createRandomWordGenerator(5), 
     fitnessFunction = createWordFitnessFunction("HELLO");
 
-// run_ga(numGens, genIndFunc, popSize, fitnessFunction);
+run_ga(numGens, genIndFunc, popSize, fitnessFunction);
 
