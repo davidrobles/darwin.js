a0974ab9bf6dc906d521cc5dce02b1f4a41ad5b1
David Robles <drobles@gmail.com>
2014-05-25 18:34:39 -0700

    Change tabs to whitespace

--- src/selection.js
+++ src/selection.js
@@ -1,15 +1,15 @@
 Darwin.Selection = {
-	randomTopPercent: function(evaluatedPopulation) {
-		var randIndex = Darwin.Utils.randomIntFromInterval(0, (evaluatedPopulation.length / 2) -1);
-		return evaluatedPopulation[randIndex];
-	},
-	// Also known as fitness proportionate selection
-	routletteWheelSelection: function(evalPop) {
-	},
-	tournamentSelection: function(evalPop) {
-	},
-	rankSelection: function(evalPop) {
-	},
-	boltzmannSelection: function(evalPop) {
-	},
+    randomTopPercent: function(evaluatedPopulation) {
+        var randIndex = Darwin.Utils.randomIntFromInterval(0, (evaluatedPopulation.length / 2) -1);
+        return evaluatedPopulation[randIndex];
+    },
+    // Also known as fitness proportionate selection
+    routletteWheelSelection: function(evalPop) {
+    },
+    tournamentSelection: function(evalPop) {
+    },
+    rankSelection: function(evalPop) {
+    },
+    boltzmannSelection: function(evalPop) {
+    },
 };
