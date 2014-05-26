3809a19c24d40dd84b4114484f2b549ae1088df5
David Robles <drobles@gmail.com>
2014-05-26 00:06:57 -0700

    Move randomCharacterMutation method to StringEvolver namespace

--- samples/string-evolver/stringEvolver.js
+++ samples/string-evolver/stringEvolver.js
@@ -37,5 +37,18 @@ var StringEvolver = {
                 $(".generations").append(generationRowView.render().el);
                 break;
         }
+    },
+    randomCharacterMutation: function(candidate) {
+        var newString = "";
+        var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "; // fix this
+        for (var i = 0; i < candidate.length; i++) {
+            if (Math.random() < 0.01) {
+                var chara = Darwin.Utils.randomIntFromInterval(0, alphabet.length);
+                newString += alphabet.charAt(chara);
+            } else {
+                newString += candidate.charAt(i);
+            }
+        }
+        return newString;
     }
 };
