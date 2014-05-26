98f8b568f5d0488945ca905e768d0d22a042038b
David Robles <drobles@gmail.com>
2014-05-25 18:33:55 -0700

    Change tabs to whitespace

--- src/operators.js
+++ src/operators.js
@@ -1,31 +1,31 @@
 Darwin.Operators = {
-	singlePointCrossover: function(parentA, parentB) {
-		// verify certain initial condition
-		var parentLength = parentA.length;
-		var crossoverPoint = Darwin.Utils.randomIntFromInterval(0, parentLength);
-		var childA = parentA.substr(0, crossoverPoint) + parentB.substr(crossoverPoint, parentLength);
-		var childB = parentB.substr(0, crossoverPoint) + parentA.substr(crossoverPoint, parentLength);
-		return {
-			childA: childA,
-			childB: childB
-		};
-	},
-	twoPointCrossover: function(parentA, parentB) {
+    singlePointCrossover: function(parentA, parentB) {
+        // verify certain initial condition
+        var parentLength = parentA.length;
+        var crossoverPoint = Darwin.Utils.randomIntFromInterval(0, parentLength);
+        var childA = parentA.substr(0, crossoverPoint) + parentB.substr(crossoverPoint, parentLength);
+        var childB = parentB.substr(0, crossoverPoint) + parentA.substr(crossoverPoint, parentLength);
+        return {
+            childA: childA,
+            childB: childB
+        };
+    },
+    twoPointCrossover: function(parentA, parentB) {
 
-	},
-	cutAndSplice: function(parentA, parentB) {
-	},
-	randomCharacterMutation: function(candidate) {
-		var newString = "";
-		var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "; // fix this
-		for (var i = 0; i < candidate.length; i++) {
-			if (Math.random() < 0.01) {
-				var chara = Darwin.Utils.randomIntFromInterval(0, alphabet.length);
-				newString += alphabet.charAt(chara);
-			} else {
-				newString += candidate.charAt(i);
-			}
-		}
-		return newString;
-	}
+    },
+    cutAndSplice: function(parentA, parentB) {
+    },
+    randomCharacterMutation: function(candidate) {
+        var newString = "";
+        var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "; // fix this
+        for (var i = 0; i < candidate.length; i++) {
+            if (Math.random() < 0.01) {
+                var chara = Darwin.Utils.randomIntFromInterval(0, alphabet.length);
+                newString += alphabet.charAt(chara);
+            } else {
+                newString += candidate.charAt(i);
+            }
+        }
+        return newString;
+    }
 };
