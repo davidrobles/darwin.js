34433c776517c44189b61ee7bfdd9f70712f9cc4
David Robles <drobles@gmail.com>
2014-06-01 01:24:50 -0700

    Create Gruntfile.js with jasmine runner

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        jasmine: {
+            src: "src/**/*.js",
+            specs: "test/**/*spec.js",
+            helpers: "test/helpers/*.js"
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-jasmine");
+    grunt.registerTask("default", "jasmine");
+}
