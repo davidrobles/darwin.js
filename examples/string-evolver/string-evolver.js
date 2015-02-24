449e027721220397e92e9c970faf6635518cec49
David Robles <drobles@gmail.com>
2015-02-23 21:41:56 -0800

    Add StringEvolver.createStringDiffView

--- examples/string-evolver/string-evolver.js
+++ examples/string-evolver/string-evolver.js
@@ -36,6 +36,29 @@ var StringEvolver = {
             }
         }
         return newString;
+    },
+
+    createStringDiffView: function(target) {
+        return Backbone.View.extend({
+
+            tagName: "p",
+
+            initialize: function(opts) { // Change options in the other views!
+                this.actual = opts.actual;
+                this.target = target;
+            },
+
+            render: function() {
+                for (var i = 0; i < this.target.length; i++) {
+                    if (this.target.charAt(i) === this.actual.charAt(i)) {
+                        this.$el.append('<span style="color: rgb(141, 199, 63);">' + this.actual.charAt(i) + '</span>');
+                    } else {
+                        this.$el.append('<span style="color: rgb(237, 28, 36);">' + this.actual.charAt(i) + "</span>");
+                    }
+                }
+                return this;
+            }
+        });
     }
 
 };
