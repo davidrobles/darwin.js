403c5ad6d076bfe955a323b496a1530d280b80fb
David Robles <drobles@gmail.com>
2015-02-27 20:11:53 -0800

    Move graph static content to renderBase

--- src/darwin-ui.js
+++ src/darwin-ui.js
@@ -326,69 +326,75 @@ var EAGraph = Backbone.View.extend({
 
     className: "widget",
 
-    render: function() {
+    initialize: function() {
+        this.renderBase();
+    },
+
+    renderBase: function() {
         var margin = {top: 20, right: 20, bottom: 30, left: 50},
             width = 560 - margin.left - margin.right,
             height = 300 - margin.top - margin.bottom;
 
-        var parseDate = d3.time.format("%d-%b-%y").parse;
-
-        var x = d3.time.scale()
+        this.x = d3.time.scale()
             .range([0, width]);
 
-        var y = d3.scale.linear()
+        this.y = d3.scale.linear()
             .range([height, 0]);
 
         var xAxis = d3.svg.axis()
-            .scale(x)
+            .scale(this.x)
             .orient("bottom");
 
         var yAxis = d3.svg.axis()
-            .scale(y)
+            .scale(this.y)
             .orient("left");
+        var self = this;
 
-        var line = d3.svg.line()
-            .x(function(d) { return x(d.date); })
-            .y(function(d) { return y(d.close); });
+        this.line = d3.svg.line()
+            .x(function(d) { return self.x(d.date); })
+            .y(function(d) { return self.y(d.close); });
 
-        var svg = d3.select(this.el).append("svg")
+        this.svg = d3.select(this.el).append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+        this.svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        this.svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Price ($)");
+    },
+
+    render: function() {
+        var parseDate = d3.time.format("%d-%b-%y").parse;
+        var self = this;
         d3.tsv("/data.tsv", function(error, data) {
             data.forEach(function (d) {
                 d.date = parseDate(d.date);
                 d.close = +d.close;
             });
 
-            x.domain(d3.extent(data, function (d) {
+            self.x.domain(d3.extent(data, function (d) {
                 return d.date;
             }));
-            y.domain(d3.extent(data, function (d) {
+            self.y.domain(d3.extent(data, function (d) {
                 return d.close;
             }));
-
-            svg.append("g")
-                .attr("class", "x axis")
-                .attr("transform", "translate(0," + height + ")")
-                .call(xAxis);
-
-            svg.append("g")
-                .attr("class", "y axis")
-                .call(yAxis)
-                .append("text")
-                .attr("transform", "rotate(-90)")
-                .attr("y", 6)
-                .attr("dy", ".71em")
-                .style("text-anchor", "end")
-                .text("Price ($)");
-
-            svg.append("path")
+            self.svg.append("path")
                 .datum(data)
                 .attr("class", "line")
-                .attr("d", line);
+                .attr("d", self.line);
         });
         return this;
     }
