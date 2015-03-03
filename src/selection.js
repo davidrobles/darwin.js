cd81578ee70d1631f8341f5afddde9878980aa57
David Robles <drobles@gmail.com>
2015-03-02 21:52:29 -0800

    Refactor randomTop selection

--- src/selection.js
+++ src/selection.js
@@ -5,28 +5,32 @@ var Darwin = Darwin || {};
     "use strict";
 
     Darwin.Selection = {
-        randomTopPercent: function(population) {
-            // TODO get percentage as a parameter
-            population = _.clone(population);
-            population.sort(function(a, b) {
-                return b.fitness - a.fitness;
-            });
-            var randIndex = _.random(0, (population.length / 5) -1);
-            return population[randIndex];
+
+        createRandomTopPercent: function(percentage) {
+            return function(population) {
+                population = _.clone(population);
+                population.sort(function(a, b) {
+                    return b.fitness - a.fitness;
+                });
+                var topSize = Math.round(population.length * percentage);
+                var randIndex = _.random(0, topSize -1);
+                return population[randIndex];
+            }
         },
+
         // Also known as fitness proportionate selection
         routletteWheelSelection: function(evalPop) {
             // TODO
         },
+
         tournamentSelection: function(evalPop) {
             // TODO
         },
+
         rankSelection: function(evalPop) {
             // TODO
-        },
-        boltzmannSelection: function(evalPop) {
-            // TODO
         }
+
     };
 
 })();
