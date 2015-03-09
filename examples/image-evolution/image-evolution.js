897ec0bc7e8aadb68bafb1389324965ba5e35cd5
David Robles <drobles@gmail.com>
2015-03-08 21:28:06 -0700

    Move ImageEvolution.createRandomImageFromColorPool to image-evolution.js

--- examples/image-evolution/image-evolution.js
+++ examples/image-evolution/image-evolution.js
@@ -11,6 +11,21 @@ var ImageEvolution = {
             }
             return fitness;
         }
+    },
+
+    createRandomImageFromColorPool: function(colorsPool, numPixels) {
+        return function() {
+            var data = [];
+            for (var i = 0; i < numPixels; i++) {
+                var colorIndex = Math.floor(Math.random() * colorsPool.length);
+                var randomColor = colorsPool[colorIndex];
+                data[i * 4] = randomColor[0];
+                data[i * 4 + 1] = randomColor[1];
+                data[i * 4 + 2] = randomColor[2];
+                data[i * 4 + 3] = randomColor[3];
+            }
+            return data;
+        };
     }
 
 };
