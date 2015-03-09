02f0f4d450264d818f6df24696e23a8bd5450e22
David Robles <drobles@gmail.com>
2015-03-08 21:22:21 -0700

    Refactor ImageEvolution example

---
+++ examples/image-evolution/image-evolution.js
@@ -0,0 +1,16 @@
+var ImageEvolution = {
+
+    createImagefitnessFunction: function(targetFitness, imageData, numPixels) {
+        return function (individual) {
+            var fitness = targetFitness;
+            for (var i = 0; i < numPixels; i++) {
+                fitness -= Math.abs(individual.genotype[i * 4] - imageData.data[i * 4]);
+                fitness -= Math.abs(individual.genotype[i * 4 + 1] - imageData.data[i * 4 + 1]);
+                fitness -= Math.abs(individual.genotype[i * 4 + 2] - imageData.data[i * 4 + 2]);
+                fitness -= Math.abs(individual.genotype[i * 4 + 3] - imageData.data[i * 4 + 3]);
+            }
+            return fitness;
+        }
+    }
+
+};
