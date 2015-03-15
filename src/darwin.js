d547357f92ad4f8e978579170709137b01af6942
David Robles <drobles@gmail.com>
2015-03-15 09:09:50 -0700

    Move generateStats() to Abstract EA class and delete utils.js

--- src/darwin.js
+++ src/darwin.js
@@ -55,7 +55,7 @@ var Darwin = Darwin || {};
         },
 
         computeStats: function() {
-            _.extend(this.currentGeneration, Darwin.Utils.generateStats(this.population));
+            _.extend(this.currentGeneration, this.generateStats());
             this.trigger("stats");
         },
 
@@ -86,6 +86,37 @@ var Darwin = Darwin || {};
                     genotype: this.individualFactory()
                 };
             }, this);
+        },
+
+        // TODO assumes the best fitness is a high value (not always true!)
+        // TODO what if more than one individual have the best fitness?
+        generateStats: function() {
+            var population = this.population
+            var totalFitness = 0,
+                bestIndividual = null,
+                worstIndividual = null;
+            _.each(population, function(individual) {
+                totalFitness += individual.fitness;
+                if (!bestIndividual || individual.fitness > bestIndividual.fitness) {
+                    bestIndividual = individual;
+                }
+                if (!worstIndividual || individual.fitness < worstIndividual.fitness) {
+                    worstIndividual = individual;
+                }
+            });
+            var average = totalFitness / population.length;
+            var diff = _.reduce(population, function(memo, individual) {
+                return memo + Math.pow(individual.fitness - average, 2)
+            }, 0);
+            var variance = diff / population.length;
+            var std = Math.sqrt(variance);
+            return {
+                fitnessAvg: average,
+                fitnessStd: std,
+                bestIndividual: bestIndividual,
+                worstIndividual: worstIndividual,
+                population: population
+            };
         },
 
         // Abstract methods
